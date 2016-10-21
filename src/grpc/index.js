119e353ff0c6338845405061ecb76a274f73c0c9
焦哥 <jiaoyingjun@quancheng-ec.com>
2016-10-21 15:16:33 +0800

    fix

---
+++ src/grpc/index.js
@@ -0,0 +1,45 @@
+/**
+ * Created by joe on 2016/10/20.
+ */
+
+
+var consul = require('consul')({
+    host: 'daily.quancheng-ec.com',
+    port: '8500'
+});
+
+
+
+consul.catalog.service.nodes('Saluki_jimmy', function(err, result) {
+    if (err) throw err;
+
+    //console.log(result);
+
+    const services = {};
+    result.forEach(function (s) {
+        services[s.ServiceID]=s;
+    })
+
+
+    consul.agent.check.list(function(err, cresult) {
+        if (err) throw err;
+
+
+        for(var i in cresult){
+            const name = i.replace('service:',"");
+            if(services[name]&&cresult[i].Status==='passing'){
+                console.log(name,services[name]);
+
+                const serviceDef = services[name];
+
+                decodeURIComponent(serviceDef.ServiceTags[0]);
+
+            }
+
+        }
+
+
+    });
+
+
+});
