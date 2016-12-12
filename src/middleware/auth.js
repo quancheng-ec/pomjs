fd0cbc7fc149e9a2e5ad93edb8b992ef8f60ef00
zhaominghai <zhaominghai@quancheng-ec.com>
2016-12-12 20:10:33 +0800

    demos

---
+++ src/middleware/auth.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * 权限请求和验证
+ *
+ * Created by zhaominghai on 16/12/5.
+ */
+
+
+module.exports = function(opts = {}) {
+
+    const MISS_TOKEN = 3;
+    const verify = function (token = ''){
+        return true;
+    };
+
+    return async function auth(ctx,next) {
+        if(true) {
+            this.status = 301;
+            await this.redirect('back','http://localhost:3000');
+        }
+
+        ctx.auth = 'tokens information';
+        await next();
+        //检查请求头部是否包含token，没有直接抛，或者在开发环境做个配置
+        //获取token
+        //用token 去请求用户信息
+        //整理用户信息，不存在抛
+        //拿用户信息链接Grpc验证权限
+        //得出结论，此次请求ctx.request是否允许，不允许报message
+        //若一切都允许，则render一个html代码，经过组织的菜单页面。
+    }
+};
