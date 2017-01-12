ec22b9979d47ec47bce650be11ce27a7d1914cd0
焦哥 <jiaoyingjun@quancheng-ec.com>
2017-01-12 23:17:16 +0800

    add middleware

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ const serve = require('koa-static');
 var root = {};
 
 async function middleware(opts) {
-    await require('./grpc/index').init(opts);
+  await require('./grpc/index').init(opts);
 }
 
 /**
@@ -38,89 +38,97 @@ async function middleware(opts) {
  * @param opts
  */
 function mergeEnv(opts) {
-    const env = process.env;
-    //用环境变量替换当前配置
-    for (let i in env) {
-        if (i.startsWith('pomjs_')) {
-            const config = i.substring(6);
-            if (config.indexOf('_') == -1) {
-                opts[config] = env[i];
-            } else {
-                let temp = 'opts';
-                let vs = config.split('_');
-                // 替换 xxx_xxx_xxx --> {'xxx':{'xxx':'xxx'}}
-                for (let index = 0; index < vs.length; index++) {
-                    temp += '.' + vs[index];
-                    if (!eval(temp) || index === vs.length - 1) {
-                        const tempValue = index < vs.length - 1 ? '{}' : "'" + env[i] + "'";
-                        eval(temp + '=' + tempValue);
-                    }
-                }
-            }
+  const env = process.env;
+  //用环境变量替换当前配置
+  for (let i in env) {
+    if (i.startsWith('pomjs_')) {
+      const config = i.substring(6);
+      if (config.indexOf('_') == -1) {
+        opts[config] = env[i];
+      } else {
+        let temp = 'opts';
+        let vs = config.split('_');
+        // 替换 xxx_xxx_xxx --> {'xxx':{'xxx':'xxx'}}
+        for (let index = 0; index < vs.length; index++) {
+          temp += '.' + vs[index];
+          if (!eval(temp) || index === vs.length - 1) {
+            const tempValue = index < vs.length - 1 ? '{}' : "'" + env[i] + "'";
+            eval(temp + '=' + tempValue);
+          }
         }
+      }
     }
+  }
 }
 
 module.exports = function (opts = {}) {
-    if (!opts.root) {
-        const index = __dirname.indexOf('node_modules');
-        if (index != -1) {
-            opts.root = __dirname.substring(0, index);
-        } else {
-            throw new Error('the opts.root can not null');
-        }
+  if (!opts.root) {
+    const index = __dirname.indexOf('node_modules');
+    if (index != -1) {
+      opts.root = __dirname.substring(0, index);
+    } else {
+      throw new Error('the opts.root can not null');
     }
-    mergeEnv(opts);
-    console.log('start pomjs with config:', opts);
-    root = opts.root;
-    const staticPath = opts.static || Path.join(root, 'static');
+  }
+  mergeEnv(opts);
+  console.log('start pomjs with config:', opts);
+  root = opts.root;
+  const staticPath = opts.static || Path.join(root, 'static');
 
-    middleware(opts);
+  middleware(opts);
 
-    app.use(convert(serve(staticPath, {maxage: 60 * 60 * 24 * 365})));
-    // set the session keys
-    app.keys = ['qc'];
-    // add session support
-    app.use(session({
-        key: "SESSIONID"   //default "koa:sess"
-    }));
+  app.use(convert(serve(staticPath, {maxage: 60 * 60 * 24 * 365})));
+  // set the session keys
+  app.keys = ['qc'];
+  // add session support
+  app.use(session({
+    key: "SESSIONID"   //default "koa:sess"
+  }));
 
-    // add multipart/form-data parsing
-    app.use(multer(opts.uploadConfig || {}));
+  // add multipart/form-data parsing
+  app.use(multer(opts.uploadConfig || {}));
 
-    // add body parsing
-    app.use(bodyParser());
+  // add body parsing
+  app.use(bodyParser());
 
-    // add the CSRF middleware
-    app.use(new CSRF(Object.assign({
-        invalidSessionSecretMessage: 'Invalid session secret',
-        invalidSessionSecretStatusCode: 403,
-        invalidTokenMessage: 'Invalid CSRF token',
-        invalidTokenStatusCode: 403,
-        excludedMethods: ['GET', 'HEAD', 'OPTIONS'],
-        disableQuery: false
-    }, opts.csrf)));
+  // add the CSRF middleware
+  app.use(new CSRF(Object.assign({
+    invalidSessionSecretMessage: 'Invalid session secret',
+    invalidSessionSecretStatusCode: 403,
+    invalidTokenMessage: 'Invalid CSRF token',
+    invalidTokenStatusCode: 403,
+    excludedMethods: ['GET', 'HEAD', 'OPTIONS'],
+    disableQuery: false
+  }, opts.csrf)));
 
 
-    app.use(async function (ctx, next) {
-        const start = new Date();
-        await next();
-        const ms = new Date() - start;
-        ctx.set('X-Response-Time', `${ms}ms`);
+  app.use(async function (ctx, next) {
+    const start = new Date();
+    await next();
+    const ms = new Date() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+  });
+
+  app.use(error(opts));
+  app.use(saluki(opts));
+  app.use(httpWrap(opts));
+  app.use(bundle(opts));
+
+  //外接中间件
+  if (opts.middlewares) {
+    opts.middlewares.forEach(function (js) {
+        console.log(js);
+        app.use(convert(require(js)(opts)));
     });
+  }
 
-    app.use(error(opts));
-    app.use(saluki(opts));
-    app.use(httpWrap(opts));
-    app.use(bundle(opts));
-    //app.use(user());
-    app.use(route(opts));
-    app.use(render(opts));
-    let port = opts.port || 3000;
-    if (typeof port === 'string') {
-        port = parseInt(port);
-    }
-    app.listen(port);
-    console.log('listening on ', port);
+  app.use(route(opts));
+  app.use(render(opts));
+  let port = opts.port || 3000;
+  if (typeof port === 'string') {
+    port = parseInt(port);
+  }
+  app.listen(port);
+  console.log('listening on ', port);
 
 };
