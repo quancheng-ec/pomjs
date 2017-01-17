c291f627e348cf3a50708a09b1b4a83d7abc2601
焦哥 <jiaoyingjun@quancheng-ec.com>
2017-01-17 09:47:50 +0800

    fix

--- dist/grpc/client.js
+++ dist/grpc/client.js
@@ -1,72 +1,72 @@
 'use strict';
 
 var initClient = function () {
-    var _ref = _asyncToGenerator(function* (saluki) {
+  var _ref = _asyncToGenerator(function* (saluki) {
 
-        console.log('init saluki client!');
+    console.log('init saluki client!');
 
-        var root = saluki.root; //'/Users/joe/work/service-all/api/src/main/proto/';
-        glob.sync(Path.join(root, "**/*_service.proto")).forEach(function (f) {
-            var proto = grpc.load({ root: root, file: f.substring(root.length) });
-            protos = _.defaultsDeep(protos, proto);
-        });
-
-        var apis = {};
-        var groups = {};
-
-        var _loop = function _loop(i) {
-            var serviceDef = saluki.services[i];
-            var ss = serviceDef.split('@');
-            var sds = ss[0].split(':');
-            var api = {};
-
-            if (ss[1]) {
-                api.target = ss[1];
-            }
-            if (sds.length === 3) {
-                // name:group:version
-                api.name = sds[0];
-                api.group = sds[1];
-                api.version = sds[2];
-            } else if (sds.length === 2) {
-                // name:version
-                api.name = sds[0];
-                api.group = saluki.salukiGroup || 'default';
-                api.version = sds[1];
-            } else if (sds.length === 1) {
-                // name
-                api.name = sds[0];
-                api.group = saluki.salukiGroup || 'default';
-                api.version = '1.0.0';
-            }
-            var names = api.name.split('.');
-            var instances = protos;
-            names.forEach(function (n) {
-                instances = !instances ? null : instances[n];
-            });
-            if (!instances) {
-                console.error('the proto not found', serviceDef);
-                return 'continue';
-            }
-            api.methods = {};
-            api._grpc = instances;
-            api._clientPool = {};
-            apis[i] = wrapService(api);
-            groups[api.group] = true;
-        };
-
-        for (var i in saluki.services) {
-            var _ret = _loop(i);
-
-            if (_ret === 'continue') continue;
-        }
-        yield initConsuls(groups);
-        return apis;
+    var root = saluki.root; //'/Users/joe/work/service-all/api/src/main/proto/';
+    glob.sync(Path.join(root, "**/*_service.proto")).forEach(function (f) {
+      var proto = grpc.load({ root: root, file: f.substring(root.length) });
+      protos = _.defaultsDeep(protos, proto);
     });
 
-    return function initClient(_x) {
-        return _ref.apply(this, arguments);
+    var apis = {};
+    var groups = {};
+
+    var _loop = function _loop(i) {
+      var serviceDef = saluki.services[i];
+      var ss = serviceDef.split('@');
+      var sds = ss[0].split(':');
+      var api = {};
+
+      if (ss[1]) {
+        api.target = ss[1];
+      }
+      if (sds.length === 3) {
+        // name:group:version
+        api.name = sds[0];
+        api.group = sds[1];
+        api.version = sds[2];
+      } else if (sds.length === 2) {
+        // name:version
+        api.name = sds[0];
+        api.group = saluki.salukiGroup || 'default';
+        api.version = sds[1];
+      } else if (sds.length === 1) {
+        // name
+        api.name = sds[0];
+        api.group = saluki.salukiGroup || 'default';
+        api.version = '1.0.0';
+      }
+      var names = api.name.split('.');
+      var instances = protos;
+      names.forEach(function (n) {
+        instances = !instances ? null : instances[n];
+      });
+      if (!instances) {
+        console.error('the proto not found', serviceDef);
+        return 'continue';
+      }
+      api.methods = {};
+      api._grpc = instances;
+      api._clientPool = {};
+      apis[i] = wrapService(api);
+      groups[api.group] = true;
     };
+
+    for (var i in saluki.services) {
+      var _ret = _loop(i);
+
+      if (_ret === 'continue') continue;
+    }
+    yield initConsuls(groups);
+    return apis;
+  });
+
+  return function initClient(_x) {
+    return _ref.apply(this, arguments);
+  };
 }();
 
 /**
@@ -76,15 +76,15 @@ var initClient = function () {
 
 
 var initConsuls = function () {
-    var _ref2 = _asyncToGenerator(function* (groups) {
-        for (var i in groups) {
-            yield consul.initWidthGroup(i);
-        }
-    });
+  var _ref2 = _asyncToGenerator(function* (groups) {
+    for (var i in groups) {
+      yield consul.initWidthGroup(i);
+    }
+  });
 
-    return function initConsuls(_x2) {
-        return _ref2.apply(this, arguments);
-    };
+  return function initConsuls(_x2) {
+    return _ref2.apply(this, arguments);
+  };
 }();
 
 /**
@@ -108,17 +108,17 @@ var _ = require('lodash');
 var consul = require('./consul');
 
 var grpcOptions = {
-    'grpc.ssl_target_name_override': 'grpc',
-    'grpc.default_authority': 'grpc'
+  'grpc.ssl_target_name_override': 'grpc',
+  'grpc.default_authority': 'grpc'
 };
 
 var ssl_creds = grpc.credentials.createSsl(FS.readFileSync(Path.join(__dirname, '../../server.pem')));
 var creds = grpc.credentials.createInsecure();
 
 var metadataUpdater = function metadataUpdater(service_url, callback) {
-    var metadata = new grpc.Metadata();
-    metadata.set('plugin_key', 'plugin_value');
-    callback(null, metadata);
+  var metadata = new grpc.Metadata();
+  metadata.set('plugin_key', 'plugin_value');
+  callback(null, metadata);
 };
 
 var mcreds = grpc.credentials.createFromMetadataGenerator(metadataUpdater);
@@ -126,105 +126,107 @@ var combined_creds = grpc.credentials.combineChannelCredentials(ssl_creds, mcred
 
 var protos = {};
 
-function getClient(api) {
+function getClient(api, index) {
 
-    if (api.target) {
-        if (api.client) {
-            return api.client;
-        }
-        var _client = new api._grpc(api.target, combined_creds, grpcOptions);
-        api.client = _client;
-        return api.client;
+  // if (api.target) {
+  //   if (api.client) {
+  //     return api.client;
+  //   }
+  //   const client = new api._grpc(api.target, combined_creds, grpcOptions);
+  //   api.client = client;
+  //   return api.client;
+  // }
+
+  var provider = consul.getService(api);
+  if (!provider) {
+    console.error('the service provider not found', api.name);
+    return null;
+  }
+
+  var providerHosts = [];
+  provider.forEach(function (s) {
+    //匹配provide和当前的service声明，如果相同则记录下来
+    if ((s.group || 'default') === api.group && (s.version || '1.0.0') === api.version) {
+      providerHosts.push(s.host);
     }
+  });
+  if (providerHosts.length === 0) {
+    console.error('the service provider not found', api, 'please check saluki service config');
+    return null;
+  }
+  //如果有重试行为，清除 client连接缓存
+  if (index) {
+    api._clientPool = {};
+  }
+  var pool = api._clientPool;
+  var host = randomLoadbalancer(providerHosts);
+  if (pool[host]) {
+    return pool[host];
+  }
 
-    var provider = consul.getService(api);
-    if (!provider) {
-        console.error('the service provider not found', api.name);
-        return null;
-    }
-
-    var providerHosts = [];
-    provider.forEach(function (s) {
-        //匹配provide和当前的service声明，如果相同则记录下来
-        if ((s.group || 'default') === api.group && (s.version || '1.0.0') === api.version) {
-            providerHosts.push(s.host);
-        }
-    });
-    if (providerHosts.length === 0) {
-        console.error('the service provider not found', api, 'please check saluki service config');
-        return null;
-    }
-
-    var pool = api._clientPool;
-
-    var host = randomLoadbalancer(providerHosts);
-    if (pool[host]) {
-        return pool[host];
-    }
-
-    var client = new api._grpc(host, combined_creds, grpcOptions);
-    pool[host] = client;
-    client._host = host;
-    return client;
+  var client = new api._grpc(host, combined_creds, grpcOptions);
+  pool[host] = client;
+  client._host = host;
+  return client;
 }
 
 function wrapService(api) {
-    var methods = api._grpc.service.children;
-    var service = {};
-    methods.forEach(function (ins) {
-        service[ins.name] = promising(api, ins.name);
-    });
-    return service;
+  var methods = api._grpc.service.children;
+  var service = {};
+  methods.forEach(function (ins) {
+    service[ins.name] = promising(api, ins.name);
+  });
+  return service;
 }
 
 function promising(api, name) {
 
-    var invoke = function invoke(req, callback, resolve, reject, index) {
-        var client = getClient(api);
-        client[name](req, function (err, resp) {
-            if (err) {
-                var reqstr = JSON.stringify(req);
-                console.error(client._host, api.name, name, reqstr, err);
-                //如果有错误重试三次
-                // if (index < 3) {
-                //     index++;
-                //     invoke(req, callback, resolve, reject, index);
-                //     return;
-                // }
-                if (!err.message) {
-                    err.message = 'grpc invoke error:' + api.name + "." + name + reqstr;
-                }
-                reject(err);
-            } else {
-                resolve(resp);
-            }
-            if (callback) {
-                callback(err, resp);
-            }
-        });
-    };
+  var invoke = function invoke(req, callback, resolve, reject, index) {
+    var client = getClient(api, index);
+    client[name](req, function (err, resp) {
+      if (err) {
+        var reqstr = JSON.stringify(req);
+        console.error(client._host, api.name, name, reqstr, err, index || 0);
+        //如果有网络错误重试五次
+        if (err.toString().indexOf('code: 14') != -1 && index < 5) {
+          index++;
+          invoke(req, callback, resolve, reject, index);
+          return;
+        }
+        if (!err.message) {
+          err.message = 'grpc invoke error:' + api.name + "." + name + reqstr;
+        }
+        reject(err);
+      } else {
+        resolve(resp);
+      }
+      if (callback) {
+        callback(err, resp);
+      }
+    });
+  };
 
-    return function (req, callback) {
-        var index = 0;
-        return new Promise(function (resolve, reject) {
-            invoke(req, callback, resolve, reject, index);
-        });
-    };
+  return function (req, callback) {
+    var index = 0;
+    return new Promise(function (resolve, reject) {
+      invoke(req, callback, resolve, reject, index);
+    });
+  };
 }
 
 function randomLoadbalancer(providerHosts) {
-    var index = getRandomIntInclusive(0, providerHosts.length - 1);
-    var host = providerHosts[index];
-    //var client = new instances(si, grpc.credentials.createInsecure());
-    return host;
+  var index = getRandomIntInclusive(0, providerHosts.length - 1);
+  var host = providerHosts[index];
+  //var client = new instances(si, grpc.credentials.createInsecure());
+  return host;
 }
 
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 module.exports = {
-    init: initClient
+  init: initClient
 };
--- src/grpc/client.js
+++ src/grpc/client.js
@@ -10,81 +10,81 @@ const _ = require('lodash');
 const consul = require('./consul');
 
 var grpcOptions = {
-    'grpc.ssl_target_name_override': 'grpc',
-    'grpc.default_authority': 'grpc'
+  'grpc.ssl_target_name_override': 'grpc',
+  'grpc.default_authority': 'grpc'
 };
 
 var ssl_creds = grpc.credentials.createSsl(FS.readFileSync(Path.join(__dirname, '../../server.pem')));
 var creds = grpc.credentials.createInsecure();
 
 var metadataUpdater = function (service_url, callback) {
-    var metadata = new grpc.Metadata();
-    metadata.set('plugin_key', 'plugin_value');
-    callback(null, metadata);
+  var metadata = new grpc.Metadata();
+  metadata.set('plugin_key', 'plugin_value');
+  callback(null, metadata);
 };
 
 
 var mcreds = grpc.credentials.createFromMetadataGenerator(metadataUpdater);
 var combined_creds = grpc.credentials.combineChannelCredentials(
-    ssl_creds, mcreds);
+  ssl_creds, mcreds);
 
 let protos = {};
 
 
 async function initClient(saluki) {
 
-    console.log('init saluki client!');
+  console.log('init saluki client!');
 
-    const root = saluki.root;//'/Users/joe/work/service-all/api/src/main/proto/';
-    glob.sync(Path.join(root, "**/*_service.proto")).forEach(function (f) {
-        const proto = grpc.load({root: root, file: f.substring(root.length)});
-        protos = _.defaultsDeep(protos, proto);
-    });
+  const root = saluki.root;//'/Users/joe/work/service-all/api/src/main/proto/';
+  glob.sync(Path.join(root, "**/*_service.proto")).forEach(function (f) {
+    const proto = grpc.load({root: root, file: f.substring(root.length)});
+    protos = _.defaultsDeep(protos, proto);
+  });
 
-    const apis = {};
-    const groups = {};
-    for (let i in saluki.services) {
-        const serviceDef = saluki.services[i];
-        const ss = serviceDef.split('@');
-        const sds = ss[0].split(':');
-        const api = {};
+  const apis = {};
+  const groups = {};
+  for (let i in saluki.services) {
+    const serviceDef = saluki.services[i];
+    const ss = serviceDef.split('@');
+    const sds = ss[0].split(':');
+    const api = {};
 
-        if (ss[1]) {
-            api.target = ss[1];
-        }
-        if (sds.length === 3) {
-            // name:group:version
-            api.name = sds[0];
-            api.group = sds[1];
-            api.version = sds[2];
-        } else if (sds.length === 2) {
-            // name:version
-            api.name = sds[0];
-            api.group = saluki.salukiGroup || 'default';
-            api.version = sds[1];
-        } else if (sds.length === 1) {
-            // name
-            api.name = sds[0];
-            api.group = saluki.salukiGroup || 'default';
-            api.version = '1.0.0';
-        }
-        const names = api.name.split('.');
-        let instances = protos;
-        names.forEach(function (n) {
-            instances = !instances ? null : instances[n];
-        });
-        if (!instances) {
-            console.error('the proto not found', serviceDef);
-            continue;
-        }
-        api.methods = {};
-        api._grpc = instances;
-        api._clientPool = {};
-        apis[i] = wrapService(api);
-        groups[api.group] = true;
+    if (ss[1]) {
+      api.target = ss[1];
     }
-    await initConsuls(groups);
-    return apis;
+    if (sds.length === 3) {
+      // name:group:version
+      api.name = sds[0];
+      api.group = sds[1];
+      api.version = sds[2];
+    } else if (sds.length === 2) {
+      // name:version
+      api.name = sds[0];
+      api.group = saluki.salukiGroup || 'default';
+      api.version = sds[1];
+    } else if (sds.length === 1) {
+      // name
+      api.name = sds[0];
+      api.group = saluki.salukiGroup || 'default';
+      api.version = '1.0.0';
+    }
+    const names = api.name.split('.');
+    let instances = protos;
+    names.forEach(function (n) {
+      instances = !instances ? null : instances[n];
+    });
+    if (!instances) {
+      console.error('the proto not found', serviceDef);
+      continue;
+    }
+    api.methods = {};
+    api._grpc = instances;
+    api._clientPool = {};
+    apis[i] = wrapService(api);
+    groups[api.group] = true;
+  }
+  await initConsuls(groups);
+  return apis;
 }
 
 /**
@@ -92,9 +92,9 @@ async function initClient(saluki) {
  * @param apis
  */
 async function initConsuls(groups) {
-    for (let i in groups) {
-        await consul.initWidthGroup(i);
-    }
+  for (let i in groups) {
+    await consul.initWidthGroup(i);
+  }
 }
 
 /**
@@ -102,106 +102,108 @@ async function initConsuls(groups) {
  * @param api
  * @returns {*}
  */
-function getClient(api) {
+function getClient(api, index) {
 
-    if (api.target) {
-        if (api.client) {
-            return api.client;
-        }
-        const client = new api._grpc(api.target, combined_creds, grpcOptions);
-        api.client = client;
-        return api.client;
+  // if (api.target) {
+  //   if (api.client) {
+  //     return api.client;
+  //   }
+  //   const client = new api._grpc(api.target, combined_creds, grpcOptions);
+  //   api.client = client;
+  //   return api.client;
+  // }
+
+  const provider = consul.getService(api);
+  if (!provider) {
+    console.error('the service provider not found', api.name);
+    return null;
+  }
+
+  const providerHosts = [];
+  provider.forEach(function (s) {
+    //匹配provide和当前的service声明，如果相同则记录下来
+    if ((s.group || 'default') === api.group && (s.version || '1.0.0') === api.version) {
+      providerHosts.push(s.host);
     }
+  });
+  if (providerHosts.length === 0) {
+    console.error('the service provider not found', api, 'please check saluki service config');
+    return null;
+  }
+  //如果有重试行为，清除 client连接缓存
+  if (index) {
+    api._clientPool = {};
+  }
+  const pool = api._clientPool;
+  const host = randomLoadbalancer(providerHosts);
+  if (pool[host]) {
+    return pool[host];
+  }
 
-    const provider = consul.getService(api);
-    if (!provider) {
-        console.error('the service provider not found', api.name);
-        return null;
-    }
-
-    const providerHosts = [];
-    provider.forEach(function (s) {
-        //匹配provide和当前的service声明，如果相同则记录下来
-        if ((s.group || 'default') === api.group && (s.version || '1.0.0') === api.version) {
-            providerHosts.push(s.host);
-        }
-    });
-    if (providerHosts.length === 0) {
-        console.error('the service provider not found', api, 'please check saluki service config');
-        return null;
-    }
-
-    const pool = api._clientPool;
-
-    const host = randomLoadbalancer(providerHosts);
-    if (pool[host]) {
-        return pool[host];
-    }
-
-    const client = new api._grpc(host, combined_creds, grpcOptions);
-    pool[host] = client;
-    client._host =  host;
-    return client;
+  const client = new api._grpc(host, combined_creds, grpcOptions);
+  pool[host] = client;
+  client._host = host;
+  return client;
 }
 
 function wrapService(api) {
-    const methods = api._grpc.service.children;
-    const service = {};
-    methods.forEach(function (ins) {
-        service[ins.name] = promising(api, ins.name)
-    });
-    return service;
+  const methods = api._grpc.service.children;
+  const service = {};
+  methods.forEach(function (ins) {
+    service[ins.name] = promising(api, ins.name)
+  });
+  return service;
 }
 
 function promising(api, name) {
 
-    const invoke = function (req, callback, resolve, reject, index) {
-        let client = getClient(api);
-        client[name](req, function (err, resp) {
-            if (err) {
-                const reqstr = JSON.stringify(req);
-                console.error(client._host,api.name,name,reqstr,err);
-                //如果有错误重试三次
-                // if (index < 3) {
-                //     index++;
-                //     invoke(req, callback, resolve, reject, index);
-                //     return;
-                // }
-                if (!err.message) {
-                    err.message = 'grpc invoke error:' + api.name + "." + name + reqstr;
-                }
-                reject(err);
-            } else {
-                resolve(resp);
-            }
-            if (callback) {
-                callback(err, resp);
-            }
-        });
-    };
+  const invoke = function (req, callback, resolve, reject, index) {
+    let client = getClient(api, index);
+    client[name](req, function (err, resp) {
+      if (err) {
+        const reqstr = JSON.stringify(req);
+        console.error(client._host, api.name, name, reqstr, err, index || 0);
+        //如果有网络错误重试五次
+        if (err.toString().indexOf('code: 14') != -1 && index < 5) {
+          index++;
+          invoke(req, callback, resolve, reject, index);
+          return;
+        }
+        if (!err.message) {
+          err.message = 'grpc invoke error:' + api.name + "." + name + reqstr;
+        }
+        reject(err);
+      } else {
+        resolve(resp);
+      }
+      if (callback) {
+        callback(err, resp);
+      }
+    });
+  };
 
-    return function (req, callback) {
-        let index = 0;
-        return new Promise(function (resolve, reject) {
-            invoke(req, callback, resolve, reject, index);
-        });
-    };
+  return function (req, callback) {
+    let index = 0;
+    return new Promise(function (resolve, reject) {
+      invoke(req, callback, resolve, reject, index);
+    });
+  };
 }
 
 function randomLoadbalancer(providerHosts) {
-    const index = getRandomIntInclusive(0, providerHosts.length - 1);
-    const host = providerHosts[index];
-    //var client = new instances(si, grpc.credentials.createInsecure());
-    return host;
+  const index = getRandomIntInclusive(0, providerHosts.length - 1);
+  const host = providerHosts[index];
+  //var client = new instances(si, grpc.credentials.createInsecure());
+  return host;
 }
 
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
 module.exports = {
-    init: initClient
+  init: initClient
 };
