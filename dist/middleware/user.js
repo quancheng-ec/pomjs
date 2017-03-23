28cd44d3b9fd9c8ff29495fadaa9aed39afcddc0
焦哥 <jiaoyingjun@quancheng-ec.com>
2017-03-23 12:48:03 +0800

    fix

--- dist/middleware/user.js
+++ dist/middleware/user.js
@@ -11,36 +11,37 @@ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, a
 //import uid from "uid-safe";
 
 //const sidKey = 'JSESSIONID';
+var pathToRegexp = require('path-to-regexp');
 
 module.exports = function () {
     var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
-
+    var auth = opts.auth || {};
+    var pathRegexps = [];
+    if (auth.regexps) {
+        auth.regexps.forEach(function (path) {
+            pathRegexps.push(pathToRegexp(path, []));
+        });
+    }
     return function () {
         var _ref = _asyncToGenerator(function* (ctx, next) {
+            init(ctx);
 
-            // if (ctx.path === '/login') {
-            //
-            //     const userName = ctx.request.body.userName;
-            //
-            // }
-
-
-            // ctx.session = {};
-            // console.log(ctx.session.userID);
-
-            //if (!ctx.session.userID) {
-            //ctx.session.userID = '123456';//{userId: '12345', time: (new Date()).timestamp};
-            // }
-
-            if (ctx.session.userId) {
-                ctx.response.append('userid', ctx.session.userId);
-            }
-            if (ctx.session.accountId) {
-                ctx.response.append('accountid', ctx.session.accountId);
-            }
-            if (ctx.session.companyId) {
-                ctx.response.append('companyid', ctx.session.companyId);
+            if (!ctx.response.header.token && pathRegexps.length > 0) {
+                for (var i = 0; i < pathRegexps.length; i++) {
+                    var re = pathRegexps[i];
+                    var rs = re.exec(ctx.originalUrl);
+                    if (rs) {
+                        yield next();
+                        return;
+                    }
+                }
+                // 没有命中白名单，进行登陆页面跳转
+                var target = encodeURIComponent(ctx.href);
+                var url = ctx.querystring ? auth.loginUrl + '&' : auth.loginUrl + '?';
+                url += 'target=' + target;
+                ctx.redirect(url);
+                return;
             }
 
             yield next();
@@ -53,3 +54,21 @@ module.exports = function () {
         return user;
     }();
 };
+/**
+ * 初始化 登陆信息，放到header里面
+ * @param ctx
+ */
+function init(ctx) {
+    if (ctx.session.token) {
+        ctx.response.append('token', ctx.session.token);
+    }
+    if (ctx.session.userId) {
+        ctx.response.append('userid', ctx.session.userId);
+    }
+    if (ctx.session.accountId) {
+        ctx.response.append('accountid', ctx.session.accountId);
+    }
+    if (ctx.session.companyId) {
+        ctx.response.append('companyid', ctx.session.companyId);
+    }
+}
--- src/middleware/user.js
+++ src/middleware/user.js
@@ -7,37 +7,56 @@
 //import uid from "uid-safe";
 
 //const sidKey = 'JSESSIONID';
+const pathToRegexp = require('path-to-regexp');
 
 module.exports = function (opts = {}) {
-
+    const auth = opts.auth || {};
+    let pathRegexps = [];
+    if (auth.regexps) {
+        auth.regexps.forEach(path => {
+            pathRegexps.push(pathToRegexp(path, []));
+        });
+    }
     return async function user(ctx, next) {
+        init(ctx);
 
-
-        // if (ctx.path === '/login') {
-        //
-        //     const userName = ctx.request.body.userName;
-        //
-        // }
-
-
-        // ctx.session = {};
-       // console.log(ctx.session.userID);
-
-        //if (!ctx.session.userID) {
-        //ctx.session.userID = '123456';//{userId: '12345', time: (new Date()).timestamp};
-        // }
-
-        if(ctx.session.userId){
-            ctx.response.append('userid', ctx.session.userId);
-        }
-        if(ctx.session.accountId){
-           ctx.response.append('accountid', ctx.session.accountId);
-        }
-        if(ctx.session.companyId){
-          ctx.response.append('companyid', ctx.session.companyId);
+        if (!ctx.response.header.token && pathRegexps.length > 0) {
+            for (let i = 0; i < pathRegexps.length; i++) {
+                let re = pathRegexps[i];
+                const rs = re.exec(ctx.originalUrl);
+                if (rs) {
+                    await  next();
+                    return;
+                }
+            }
+            // 没有命中白名单，进行登陆页面跳转
+            const target = encodeURIComponent(ctx.href);
+            let url = ctx.querystring ? auth.loginUrl + '&' : auth.loginUrl + '?';
+            url += 'target=' + target;
+            ctx.redirect(url);
+            return;
         }
 
+        await  next();
 
-        await next();
+
+    }
+}
+/**
+ * 初始化 登陆信息，放到header里面
+ * @param ctx
+ */
+function init(ctx) {
+    if (ctx.session.token) {
+        ctx.response.append('token', ctx.session.token);
+    }
+    if (ctx.session.userId) {
+        ctx.response.append('userid', ctx.session.userId);
+    }
+    if (ctx.session.accountId) {
+        ctx.response.append('accountid', ctx.session.accountId);
+    }
+    if (ctx.session.companyId) {
+        ctx.response.append('companyid', ctx.session.companyId);
     }
 }
