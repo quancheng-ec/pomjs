ad6514687b5b19e477294f2993a146300324755b
zephyr <zhangxin@quancheng-ec.com>
2017-08-17 15:25:12 +0800

    add services to app.context

--- src/middleware/saluki2.js
+++ src/middleware/saluki2.js
@@ -3,7 +3,7 @@ import Saluki2Client from '@quancheng/saluki2-node'
 export default function (app, opts) {
   const client = new Saluki2Client(opts)
   client.init()
-  app.context.services = client.services
+  app.services = client.services
   return async (ctx, next) => {
     ctx.services = client.services
     await next()
