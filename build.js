1833d3b6a5853861bd2149c2768fb89ba58f8e54
zephyr <zhangxin@quancheng-ec.com>
2017-11-03 11:44:24 +0800

    update

--- build.js
+++ build.js
@@ -2,7 +2,7 @@
  * Created by joe on 2016/10/17.
  */
 
-const pageLoader = require('./util/pageLoader')
+const pageLoader = require('./src/util/pageLoader')
 const fs = require('fs-sync')
 
 module.exports = async function (opts = {}) {
