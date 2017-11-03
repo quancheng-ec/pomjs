0646694d0355b86fd23c346dee440e41108b91e6
zephyr <zhangxin@quancheng-ec.com>
2017-11-03 16:24:43 +0800

    add 404 page

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -1,27 +1,19 @@
-/**
- *
- * 系统错误处理
- *
- * Created by joe on 16/10/13.
- */
-
-
-
-module.exports = function (opts = {}) {
-
-
-    return async function error(ctx, next) {
-
-        try {
-            await next();
-        } catch (err) {
-            ctx.status = err.statusCode || err.status || 500;
-            ctx.body = {
-                error: err.message
-            };
-            if (ctx.status !== 404) {
-                console.error(err);
-            }
-        }
+const fs = require('fs-extra')
+const { resolve } = require('path')
+module.exports = function(opts = {}) {
+  return async function error(ctx, next) {
+    try {
+      await next()
+    } catch (err) {
+      ctx.status = err.statusCode || err.status || 500
+      ctx.type = 'html'
+      ctx.body = await fs.readFile(
+        resolve(__dirname, '../../views/error.html'),
+        'utf8'
+      )
+      if (ctx.status !== 404) {
+        console.error(err)
+      }
     }
+  }
 }
