42878dddeb12d7fa107cdddc1a7954e304181570
zephyr <zhangxin@quancheng-ec.com>
2018-02-28 14:07:37 +0800

    update

--- client.js
+++ client.js
@@ -2,44 +2,43 @@
  * Created by joe on 2016/10/14.
  */
 
-var  Vue = require('vue');
-var  VueResource = require('vue-resource');
-var fetch =require('whatwg-fetch');
+var Vue = require('vue')
+var VueResource = require('vue-resource')
+var fetch = require('whatwg-fetch')
 
 //const Url = require('browser-url');
 
-Vue.use(VueResource);
+Vue.use(VueResource)
 
 var fetchPlugin = {
-    install: function (Vue, options) {
-        Vue.fetch = fetch;
-        Vue.prototype.fetch = fetch;
-    }
+  install: function(Vue, options) {
+    Vue.fetch = fetch
+    Vue.prototype.fetch = fetch
+  }
 }
 
-Vue.use(fetchPlugin);
+Vue.use(fetchPlugin)
 
-var context = __vue_context_data;
+var context = __vue_context_data
 
-Vue.http.headers.common["X-CSRF-Token"] = context.csrf
+Vue.http.headers.common['X-CSRF-Token'] = context.csrf
 
-var view = require('./' + context.pageContext.pageAction + '.vue');
+var view = require('./' + context.pageContext.pageAction + '.vue')
 
-if(view.mixins){
+if (view.mixins) {
   view.mixins.push({
     data: function() {
-      return context;
+      return context
     }
   })
-}else{
-  view.mixins = [{
-    data: function() {
-      return context;
+} else {
+  view.mixins = [
+    {
+      data: function() {
+        return context
+      }
     }
-  }];
+  ]
 }
 
-new Vue(view).$mount('#wrapper');
-
-
-
+new Vue(view).$mount('#wrapper')
